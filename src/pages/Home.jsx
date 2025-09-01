a18d4b20033625ffda6bd9b6c95a3ac7526aaffb
Vadym Smirnov <vadim.te180880@gmail.com>
2025-09-01 19:32:15 -0400

    Initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 // Домашня сторінка
 import { Button, Box, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
-
 export default function Home() {
   const navigate = useNavigate();
   return (
