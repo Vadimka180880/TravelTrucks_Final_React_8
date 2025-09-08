eb8104d0f0a313300663d2cffd09cef21e102bc2
Vadym Smirnov <vadim.te180880@gmail.com>
2025-09-07 22:40:39 -0400

    feat: use webp banner with jpg fallback

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.css';
-import mainPhoto from '../assets/images/banner.jpg';
-import mainPhoto2x from '../assets/images/banner@2x.jpg';
+import mainPhotoJpg from '../assets/images/banner.jpg';
+import mainPhotoWebp from '../assets/images/banner.webp';
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
         <div className={styles.heroWrapper}>
           <div className={styles.heroSection}>
-            <img
-              src={mainPhoto}
-              srcSet={`${mainPhoto} 1x, ${mainPhoto2x} 2x`}
-              alt="Campers"
-              className={styles.heroImage}
-            />
+            <picture>
+              <source srcSet={mainPhotoWebp} type="image/webp" />
+              <img src={mainPhotoJpg} alt="Campers" className={styles.heroImage} />
+            </picture>
             <div className={styles.heroContent}>
               <h1>Campers of Your Dreams</h1>
               <p>You can find everything you want in our catalog</p>
